922a6d1c7e0a500f889571670a4b8ac5b5bdb648
Kevinrocks100 <Liukevin209@gmail.com>
2023-01-06 15:44:24 -0500

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import data from 'src/assets/json/data.json'; 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'my-app';
+  appId : String; 
+  traceId : String; 
+  errorCode : String; 
+  message : String; 
+  messageTimestamp : String; 
+  constructor() {
+    this.appId = data.msgContext.appId; 
+    this.traceId = data.traceId; 
+    this.errorCode = data.additionalInfo.errorCode; 
+    this.message = data.message; 
+    this.messageTimestamp = data.additionalInfo.messageTimestamp; 
+  }
 }
